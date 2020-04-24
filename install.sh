b69451db2f2b7fce675af4ef45f2e22d407a0ed8
VWolf <58598786+VWolf13@users.noreply.github.com>
2020-04-24 19:37:28 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,11 @@
+dos2unix vnmap.sh
+pkg install nmap
+clear
+echo '___    _____       __       ______________'
+echo '__ |  / /__ |     / /______ ___  /___  __/'
+echo '__ | / / __ | /| / / _  __ \__  / __  /_  '
+echo '__ |/ /  __ |/ |/ /  / /_/ /_  /  _  __/  '
+echo '_____/   ____/|__/   \____/ /_/   /_/'
+sleep 1.6
+clear
+bash vnmap.sh
