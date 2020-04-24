96052d4f18d1ab929a8683f96423647a5be3d4d4
VWolf <58598786+VWolf13@users.noreply.github.com>
2020-04-24 20:58:19 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
-#!/bin/bash
 dos2unix vnmap.sh
 pkg install nmap
+clear
+sleep 0.5
+echo '___    _____       __       ______________'
+echo '__ |  / /__ |     / /______ ___  /___  __/'
+echo '__ | / / __ | /| / / _  __ \__  / __  /_  '
+echo '__ |/ /  __ |/ |/ /  / /_/ /_  /  _  __/  '
+echo '_____/   ____/|__/   \____/ /_/   /_/'
+sleep 0.8
 bash vnmap.sh
